b34ead388a61043438dfefe78d54e98b1edf8487
Livingpool <52132459+Livingpool@users.noreply.github.com>
2025-02-03 21:55:36 +0800

    parsing return statements

--- parser/parser.go
+++ parser/parser.go
@@ -53,6 +53,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.currToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -70,6 +72,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.currTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.currToken}
+	p.nextToken()
 
 	// TODO: We're skipping the expressions until we
 	// encounter a semicolon
