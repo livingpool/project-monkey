077b13d70ee2c9e7d515480cfd03f4718e89bd9c
Livingpool <52132459+Livingpool@users.noreply.github.com>
2025-02-04 21:18:48 +0800

    parsing integer literals

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"project-monkey/ast"
 	"project-monkey/lexer"
 	"project-monkey/token"
+	"strconv"
 )
 
 const (
@@ -39,6 +40,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// read two tokens, so currToken and peekToken are both set
 	p.nextToken()
@@ -141,6 +143,20 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.currToken}
+
+	value, err := strconv.ParseInt(p.currToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.currToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	return lit
 }
 
 func (p *Parser) currTokenIs(t token.TokenType) bool {
