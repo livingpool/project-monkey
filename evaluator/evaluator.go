30a4d8028284aea1f81d357ca3efcf5c5462f5cb
Livingpool <52132459+Livingpool@users.noreply.github.com>
2025-02-11 08:47:39 +0800

    evaluating boolean literals

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,15 @@ import (
 	"project-monkey/object"
 )
 
+// there are only 2 possible values for boolean objects!
+// so we can just reference 'em instead of creating new ones
+// every time we encounter a true or false
+// i think this is kinda like git's tree!
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// statements
@@ -16,6 +25,8 @@ func Eval(node ast.Node) object.Object {
 	// expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -29,4 +40,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
