96c8f24679616344465cb987ced5da6c52e591f8
livingpool <52132459+livingpool@users.noreply.github.com>
2025-02-23 21:34:41 +0800

    string concatenation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -199,6 +199,10 @@ func TestErrorHandling(t *testing.T) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -294,9 +298,22 @@ func TestStringLiteral(t *testing.T) {
 	evaluated := testEval(t, input)
 	str, ok := evaluated.(*object.String)
 	if !ok {
-		t.Fatalf("object is not String. got=%T (+%v)", evaluated, evaluated)
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
 	}
 	if str.Value != "Hello world!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(t, input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
